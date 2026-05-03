33b3efda7bf04a3bb1fadca88b6d9f7764fb7cc9
Alexis <ag202416673@alm.buap.mx>
2026-05-03 11:06:27 -0600

    Pequeño Cambio al contacto y script 	modified:   contacto.html 	modified:   estilo.css 	modified:   script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
+/*document.addEventListener("DOMContentLoaded", function() {
     let botonRegistrar = document.getElementById("Enviar");
+}*/
+/*
 
     botonRegistrar.addEventListener("mouseover", function() { 
         botonRegistrar.style.backgroundColor = "#198754";
@@ -8,4 +10,17 @@ document.addEventListener("DOMContentLoaded", function() {
     botonRegistrar.addEventListener("mouseout", function() { 
         botonRegistrar.style.backgroundColor = "";
     });
+});*/
+
+let formulario = document.getElementById("formulario"); 
+formulario.addEventListener("submit", function(event) { 
+    event.preventDefault(); // evita recargar 
+    let nombre = document.getElementById("nombre").value.trim(); 
+    let mail = document.getElementById("correo").value.trim(); 
+    let mensajeUsuario = document.getElementById("Message").value.trim(); 
+    if (nombre === "" || mail === "" || mensajeUsuario === "") { 
+        document.getElementById("mensaje").textContent = "Datos inválidos: Nombre, Correo y el mensaje son obligatorios."; 
+    } else { 
+        document.getElementById("mensaje").textContent = "¡Mensaje enviado con exito!"; 
+    } 
 });
